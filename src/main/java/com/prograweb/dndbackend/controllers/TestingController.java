b7330378cd69aff299cc5c448200f2748638d775
Oscariolo <oscariolopinop21@gmail.com>
2025-12-25 21:14:01 -0500

    ADD-Added docker compose with mongodb init values

--- src/main/java/com/prograweb/dndbackend/controllers/TestingController.java
+++ src/main/java/com/prograweb/dndbackend/controllers/TestingController.java
@@ -2,15 +2,14 @@ package com.prograweb.dndbackend.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
 public class TestingController {
     
     @GetMapping("/healthcheck")
-    public String getMethodName(@RequestParam String param) {
-        return new String("Service is up and running! You sent: " + param);
+    public String getMethodName() {
+        return new String("Service is up and running!");
     }
     
 
