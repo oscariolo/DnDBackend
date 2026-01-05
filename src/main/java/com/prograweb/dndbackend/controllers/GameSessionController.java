3d7a3fc117a39a32081857a37bc849ead1849a21
Oscariolo <oscariolopinop21@gmail.com>
2026-01-04 20:03:16 -0500

    ADD-Added websocket for chat, zone update and dice roll

---
+++ src/main/java/com/prograweb/dndbackend/controllers/GameSessionController.java
@@ -0,0 +1,61 @@
+package com.prograweb.dndbackend.controllers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+import com.prograweb.dndbackend.domain.dtos.GameEvents.ChatMessageDTO;
+import com.prograweb.dndbackend.domain.dtos.GameEvents.ZoneUpdateDTO;
+import com.prograweb.dndbackend.services.DnDGameService;
+
+@Controller
+@MessageMapping("/game-session/{roomId}") // 1. Prefix for INCOMING messages
+//TODO implementar que al momento de conectarse a la sala primero se recupere el estado actual de la campaña (zona actual, personajes, etc)
+public class GameSessionController {
+
+    @Autowired
+    private DnDGameService dndGameService;
+
+    @MessageMapping("/zone-update")
+    @SendTo("/topic/game-session/{roomId}/zone-update") 
+    public ZoneUpdateDTO handleGameSessionMessage(@DestinationVariable String roomId, ZoneUpdateDTO event) {
+        try {
+            return event;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null; // returning null prevents sending any message
+        }
+    }
+
+    @MessageMapping("/chat-message")
+    @SendTo("/topic/game-session/{roomId}/chat-message")
+    public ChatMessageDTO handleChatMessage(@DestinationVariable String roomId, ChatMessageDTO message) {
+        try {
+            return message;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @MessageMapping("/dice-roll")
+    @SendTo("/topic/game-session/{roomId}/dice-roll")
+    public ChatMessageDTO handleDiceRoll(@DestinationVariable String roomId, ChatMessageDTO diceRoll) {
+        try {
+            if(diceRoll.messageContent == null || diceRoll.messageContent.isEmpty()) {
+                throw new IllegalArgumentException("Dice roll expression cannot be null or empty");
+            }
+            //check if is a number
+            if(diceRoll.messageContent.matches("\\d+")) {
+                return diceRoll;
+            }else{
+                System.err.println("Invalid dice roll expression: " + diceRoll.messageContent);
+                return null;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
---
+++ src/main/java/com/prograweb/dndbackend/services/DnDGameService.java
@@ -0,0 +1,10 @@
+package com.prograweb.dndbackend.services;
+import org.springframework.stereotype.Service;
+
+
+@Service
+//TODO dndgameservice se utiliza unicamente para eventos en juego que deban ser persistidos
+//guardar progreso de los personajes de los jugadores, guardar estado de la campaña (zona actual)
+public class DnDGameService {
+
+}
