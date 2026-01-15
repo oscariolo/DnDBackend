246467d1a3d71596f8bfe5a0a44c29905b35c0c6
S4NT1EC <santiago.velezd@ucuenca.edu.ec>
2026-01-15 12:14:59 -0500

    CHANGES: se quito los final de lo que se sube las campaign, CHANGE: host de mongo, ADD: el archivo de config de CORS

--- src/main/java/com/prograweb/dndbackend/domain/dtos/CampaignUploadDTO.java
+++ src/main/java/com/prograweb/dndbackend/domain/dtos/CampaignUploadDTO.java
@@ -1,20 +1,15 @@
 package com.prograweb.dndbackend.domain.dtos;
 import java.util.List;
-
 import lombok.Data;
+import lombok.AllArgsConstructor;
 
 @Data
+@AllArgsConstructor
 public class CampaignUploadDTO {
-
-    private final String dungeonMasterId;
-
-    private final String name;
-
-    private final int maxPlayers;
-
+    private String dungeonMasterId;
+    private String name;
+    private int maxPlayers;
     private String description;
-
     private List<CampaingZoneUploadDTO> zones;
-    
 }
 
--- src/main/java/com/prograweb/dndbackend/domain/models/ImageMetaData.java
+++ src/main/java/com/prograweb/dndbackend/domain/models/ImageMetaData.java
@@ -1,7 +1,10 @@
 package com.prograweb.dndbackend.domain.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import lombok.Data;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 @Data
 public class ImageMetaData {
     private String fileNameReference;
