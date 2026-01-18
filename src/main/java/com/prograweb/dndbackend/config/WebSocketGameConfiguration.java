7b47d5d4c72a9a537a2e72db5854cf4b5c22ca54
Oscariolo <oscariolopinop21@gmail.com>
2026-01-18 14:25:36 -0500

    MOD-Gropued config files

--- src/main/java/com/prograweb/dndbackend/config/WebSocketGameConfiguration.java
+++ src/main/java/com/prograweb/dndbackend/config/WebSocketGameConfiguration.java
@@ -1,4 +1,4 @@
-package com.prograweb.dndbackend.configs;
+package com.prograweb.dndbackend.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
