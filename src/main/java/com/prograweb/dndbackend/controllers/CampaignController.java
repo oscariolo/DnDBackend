e1a6119b05a1f2d6c840c0a226e9d3d7524992b6
Oscariolo <oscariolopinop21@gmail.com>
2026-01-25 18:00:20 -0500

    DEL-Removed game event related service

--- src/main/java/com/prograweb/dndbackend/controllers/CampaignController.java
+++ src/main/java/com/prograweb/dndbackend/controllers/CampaignController.java
@@ -2,13 +2,8 @@ package com.prograweb.dndbackend.controllers;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import com.prograweb.dndbackend.domain.dtos.CampaignUploadDTO;
-import com.prograweb.dndbackend.domain.dtos.NewCampaignCharacterDTO;
-import com.prograweb.dndbackend.domain.dtos.NewPlayerDTO;
-import com.prograweb.dndbackend.domain.dtos.GameEvents.LevelUpDTO;
 import com.prograweb.dndbackend.domain.models.campaign.Campaign;
-import com.prograweb.dndbackend.domain.models.campaign.CampaignRun;
 import com.prograweb.dndbackend.services.CampaignService;
-import com.prograweb.dndbackend.services.DnDGameService;
 import jakarta.validation.Valid;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +15,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.PutMapping;
 
 
 
@@ -32,9 +26,6 @@ public class CampaignController {
 
     @Autowired
     private CampaignService campaignService;
-
-    @Autowired
-    private DnDGameService gameService;
 
     @GetMapping
     public ResponseEntity<List<Campaign>> getAllCampaigns() {
@@ -59,44 +50,6 @@ public class CampaignController {
     public ResponseEntity<Campaign> addCampaign(@Valid @RequestBody Campaign campaign) {
         campaignService.addCampaign(campaign);
         return ResponseEntity.ok(campaign);
-    }
-
-    @PostMapping("/game")
-    public CampaignRun createNewCampaignRun(@RequestBody CampaignRun newRun) {
-        return gameService.saveCampaignRun(newRun);
-    }
-
-    @PutMapping("/game/levelup")
-    public ResponseEntity<CampaignRun> levelUpCharacter(@RequestBody LevelUpDTO levelUpDto) {
-        CampaignRun updateLevel = gameService.levelUpCharacter(levelUpDto);
-        return ResponseEntity.ok(updateLevel);
-    }
-
-    @PostMapping("/game/character")
-    public ResponseEntity<CampaignRun> postGameCharacter(@RequestBody NewCampaignCharacterDTO newcharacterDto) {
-        CampaignRun campaignRun = gameService.addNewCharacterToRun(newcharacterDto);
-        if(campaignRun == null){
-            return ResponseEntity.notFound().build();
-        }
-        return ResponseEntity.ok(campaignRun);
-    }
-
-    @GetMapping("/game/{gameId}")
-    public ResponseEntity<CampaignRun> getCampaignRun(@PathVariable String gameId) {
-        CampaignRun campaignRun = gameService.getCampaignRunById(gameId);
-        return ResponseEntity.ok(campaignRun);
-    }
-    
-    @GetMapping("/game/user/playing/{userId}")
-    public ResponseEntity<List<CampaignRun>> getCampaignsPlayingByUserId(@PathVariable String userId) {
-        List<CampaignRun> campaignRuns = gameService.getCampaignsPlayingByUserId(userId);
-        return ResponseEntity.ok(campaignRuns);
-    }
-
-    @PostMapping("/game/user")
-    public ResponseEntity<String> postNewPlayerToCampaign(@RequestBody NewPlayerDTO newPlayerentity) {
-        gameService.addNewPlayerToCampaign(newPlayerentity);
-        return ResponseEntity.ok("Jugador agregado a la campaña exitosamente");
     }
 
     @GetMapping("/{campaignId}")
--- src/main/java/com/prograweb/dndbackend/domain/dtos/GameEvents/ZoneUpdateDTO.java
+++
@@ -1,7 +0,0 @@
-package com.prograweb.dndbackend.domain.dtos.GameEvents;
-
-public class ZoneUpdateDTO {
-    public String zoneName;
-    public String description;
-    public String imgUrl;
-}
